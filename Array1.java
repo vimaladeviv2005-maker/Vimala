59d5ab152b7b2ae43c931a0650f47242e80ec88a
Vimaladeviv <vimaladeviv2005@gmail.com>
2025-11-15 12:05:19 +0530

    for the program

---
+++ Array1.java
@@ -0,0 +1,13 @@
+class Array1
+{
+	public static void main(String args[])
+	{
+		System.out.println("Age of People");
+		System.out.println("______________");
+		int arr[]={25,30,18,45,50};
+		for(int i=0;i<5;i++)
+		{
+		System.out.println("people age: "+arr[i]);
+		}
+	}
+}
